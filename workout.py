0c9ea4e139bc7be4fd5493a7605c5df741dc8405
Pettmole <84863024+pettmole@users.noreply.github.com>
2022-01-12 14:31:47 -0500

    Add files via upload

---
+++ workout.py
@@ -0,0 +1,38 @@
+import time
+gaming = input("type gaming to start")
+
+if gaming == "gaming":
+    print("alright here is the list of workouts")
+    print("in two minutes I will notify you to restart")
+    print("""5 burpees
+    10 Pushups
+    15 Plank Jacks
+    20 Jump Squats
+    Rest in the time left over""")
+    time.sleep(120)
+    print("""5 burpees
+    10 Pushups
+    15 Plank Jacks
+    20 Jump Squats
+    Rest in the time left over""")
+    time.sleep(120)
+    print("""5 burpees
+    10 Pushups
+    15 Plank Jacks
+    20 Jump Squats
+    Rest in the time left over""")
+    time.sleep(120)
+    print("""5 burpees
+    10 Pushups
+    15 Plank Jacks
+    20 Jump Squats
+    Rest in the time left over""")
+    time.sleep(120)
+    print("""5 burpees
+        10 Pushups
+        15 Plank Jacks
+        20 Jump Squats
+        Rest in the time left over""")
+    time.sleep(120)
+    print("Times up all done.")
+    time.sleep(5)
